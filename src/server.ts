d094e02f2b52e1de75e4e7380e3501496f47f397
Matt Cadorette <mcadorette@gmail.com>
2019-05-08 08:43:22 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ export const app = express();
 
 app.get(["/", "/:name"], (req, res) => {
     if (req.params.name !== undefined) {
-        res.send(`Hello World May 8th NEW! And you too ${req.params.name}!`);
+        res.send(`Hello World May 8th And it's NEW! And you too ${req.params.name}!`);
     } else {
-        res.send("Hello World May 8th NEW!");
+        res.send("Hello World May 8th And it's NEW!");
     }
 });
 
